602185fae92bb1c5f55b22db1e9cd6bf4c525e98
shantanu471 <shaan2k3@gmail.com>
2025-03-17 13:19:08 +0530

    Set up react environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
-import "../stylesheets/application.scss"
+import "../stylesheets/application.scss";
+
+import { setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
+
+initializeLogger();
+setAuthHeaders();
