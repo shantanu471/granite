a1b0e1ab0270cc26ad813ae11f4bbf8a0cb705fc
shantanu471 <a.shantanu@pixelcompute.com>
2025-03-26 13:27:42 +0000

    Added show page for a task

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :tasks, only: %i[index create], param: :slug
+  resources :tasks, only: %i[index create show], param: :slug
 
   root "home#index"
   get "*path", to: "home#index", via: :all
