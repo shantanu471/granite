3750771f47bccf802b5adf45c12a91f944b4edb5
shantanu471 <a.shantanu@pixelcompute.com>
2025-03-27 07:58:36 +0000

    Added ability to delete a task

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :tasks, except: %i[new edit destroy], param: :slug
+  resources :tasks, except: %i[new edit], param: :slug
 
   root "home#index"
   get "*path", to: "home#index", via: :all
