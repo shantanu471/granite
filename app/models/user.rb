e1839c570040794fd8eff15af08d9c423c370ad1
shantanu471 <a.shantanu@pixelcompute.com>
2025-04-15 16:59:28 +0000

    Added comments to tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,12 @@ class User < ApplicationRecord
   MIN_PASSWORD_LENGTH = 6
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   MAX_EMAIL_LENGTH = 255
+
   has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
+
+  has_many :comments, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates :email, presence: true,
     uniqueness: { case_sensitive: false },
